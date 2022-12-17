59ab1fe6904d03b15d2ba3a449ed75a3a494afcc
andrei z <andreiserbanzaha@gmail.com>
2022-12-17 14:05:14 +0200

    add status header for EXIT_FAILURE and EXIT_SUCCESS

---
+++ src/status.h
@@ -0,0 +1,12 @@
+#ifndef COSM_STATUS_H__
+#define COSM_STATUS_H__
+
+namespace cosm
+{
+
+constexpr const int EXIT_SUCCESS = 1;
+constexpr const int EXIT_FAILURE = 0;
+
+} // namespace cosm
+
+#endif // COSM_STATUS_H__
